8ebb3949246de0cd5415d6f933248c7edac82bee
Hidekazu175705 <33239487+Hidekazu175705@users.noreply.github.com>
2020-01-07 17:16:57 +0900

    Add files via upload

--- show_result/show_result/show_result.swift
+++ show_result/show_result/show_result.swift
@@ -12,14 +12,16 @@ class show_result: UIViewController {
     
     var argString = ""
     var highScore:Int = 0
-    var rank: [Int] = [1]
-
+    let rankkey = "rank"
+    let HIGHSCOREKEY = "highScore"
+    
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //defaults.register(defaults: ["highScore": "0"])
+        defaults.register(defaults: [HIGHSCOREKEY:0])
+        highScore = defaults.integer(forKey: HIGHSCOREKEY)
         
         label1.text = argString
         let score = Int(argString)!
@@ -28,21 +30,34 @@ class show_result: UIViewController {
         //if(highScore == nil){
         //    highScore = 0
         //}
-        if(score>defaults.integer(forKey: "highScore")){
-            defaults.set(score, forKey: "highScore")
+        if(score>highScore){
+            defaults.set(score, forKey: HIGHSCOREKEY)
             defaults.synchronize()
             highScore = score
         }
         
-        label2.text = String(defaults.integer(forKey: "highScore"))
+        label2.text = highScore.description
         
+        /*
         rank.append(score)
         rank.sort(by: >)
         
         defaults.set(rank, forKey: "rank")
         defaults.synchronize()
         
-        Rank.text = "\(rank)"
+        Rank.text = String(defaults.integer(forKey: "rank"))
+        
+        */
+        
+        var RANK: [Int] = defaults.array(forKey: rankkey) as! [Int]
+        RANK.append(score)
+        RANK.sort(by: >)
+        if(RANK.count>3){
+            RANK.removeLast()
+        }
+        Rank.text = String(describing: RANK)
+        defaults.set(RANK, forKey: rankkey)
+        defaults.synchronize()
         
     }
 
